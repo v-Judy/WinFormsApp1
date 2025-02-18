69caf9280eb854e38807d002093dccec7e77989b
v-judyzhu <v-judyzhu@MAUIjudy>
2025-02-18 02:21:29 +0000

    Add project files.

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+          
+
+            InitializeComponent();
+
+
+        }
+    }
+}
